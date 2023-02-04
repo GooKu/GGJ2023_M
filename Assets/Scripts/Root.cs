ef35bafcbd343a870fc13467d0fab78949a89c98
OLDShaw1022 <73062433+OLDShaw1022@users.noreply.github.com>
2023-02-04 11:55:37 +0800

    change root's namespace

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -1,54 +1,57 @@
-using GGJ23M;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Root : MonoBehaviour
+namespace GGJ23M
 {
-    int _energyCost;
-    int _branchLevel;
-
-    Root _lastRoot;
-    List<Root> _childRoots;
-    Hex _rootHex;
-
-
-    public void Init(Root lastRoot, Hex rootHex, int branchLevel)
+    public class Root : MonoBehaviour
     {
-        _lastRoot = lastRoot;
-        _rootHex = rootHex;
-        _branchLevel = branchLevel;
-    }
-    public int CalculateCost()
-    {
-        //or just return branchLevel
-        switch (_branchLevel)
+        int _energyCost;
+        int _branchLevel;
+
+        Root _lastRoot;
+        List<Root> _childRoots;
+        Hex _rootHex;
+
+
+        public void Init(Root lastRoot, Hex rootHex, int branchLevel)
         {
-            case 0:
-                _energyCost = 1;
-                break;
-            default:
-                break;
+            _lastRoot = lastRoot;
+            _rootHex = rootHex;
+            _branchLevel = branchLevel;
         }
-        return _energyCost;
+        public int CalculateCost()
+        {
+            //or just return branchLevel
+            switch (_branchLevel)
+            {
+                case 0:
+                    _energyCost = 1;
+                    break;
+                default:
+                    break;
+            }
+            return _energyCost;
+        }
+
+        public Hex ReturnHex()
+        {
+            return _rootHex;
+        }
+
+        public void CreateChildRoot(Hex childRootHex)
+        {
+            //Instantiate(prefab, childRootHex, quaternion.identity, ???).addComponent(Root).Init(this, childRoothex, _branchLevel);
+            //Player.AddRoot();
+            //_childRoots.Add();
+        }
+
+        public void SeperateRoot(Hex rootHex)
+        {
+            //Instantiate(prefab, rootHex, quaternion.identity, ???).addComponent(Root).Init(this, roothex, _branchLevel + 1);
+            //Player.AddRoot();
+            //_childRoots.Add();
+        }
     }
 
-    public Hex ReturnHex()
-    {
-        return _rootHex;
-    }
-
-    public void CreateChildRoot(Hex childRootHex)
-    {
-        //Instantiate(prefab, childRootHex, quaternion.identity, ???).addComponent(Root).Init(this, childRoothex, _branchLevel);
-        //Player.AddRoot();
-        //_childRoots.Add();
-    }
-
-    public void SeperateRoot(Hex rootHex)
-    {
-        //Instantiate(prefab, rootHex, quaternion.identity, ???).addComponent(Root).Init(this, roothex, _branchLevel + 1);
-        //Player.AddRoot();
-        //_childRoots.Add();
-    }
 }
