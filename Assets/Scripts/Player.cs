aeee0d77df55c39934b1604cc6961c02a517ca8f
OLDShaw1022 <73062433+OLDShaw1022@users.noreply.github.com>
2023-02-04 11:40:34 +0800

    Add root and player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int _currentEnergy;
+
+    bool _playerDead;
+
+    List<Root> _roots;
+
+    public void AddEnergy(int addValue)
+    {
+        _currentEnergy += addValue;
+    }
+
+    public void RemoveEnergy(int removeValue)
+    {
+        _currentEnergy -= removeValue;
+
+        if(_currentEnergy <= 0)
+        {
+            _playerDead = true;
+        }
+    }
+
+    public void AddRoot(Root root) 
+    {
+        _roots.Add(root);
+    }
+
+    public List<Root> ReturnRoots()
+    {
+        return _roots;
+    }
+
+    public bool IfPlayerDead()
+    {
+        return _playerDead;
+    }
+}
