d8035de6202240ec0fcb91599a0856d2d29bbe8e
Wen Taichi <daleth90@gmail.com>
2023-02-05 10:47:23 +0800

    feat: start cutscene

---
+++ Assets/Scripts/Cutscene/GameStartProcess.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ23M
+{
+    public class GameStartProcess : MonoBehaviour
+    {
+        public event Action Finished;
+
+        [SerializeField]
+        private Animator cutsceneAnimator;
+        [SerializeField]
+        private Image image;
+        [SerializeField]
+        private List<Sprite> sprites = new();
+
+        public void Do()
+        {
+            gameObject.SetActive(true);
+            StartCoroutine(Process());
+        }
+
+        private IEnumerator Process()
+        {
+            for (var i = 0; i < sprites.Count; i++)
+            {
+                yield return PlaySpriteProcess(sprites[i]);
+            }
+
+            Finished?.Invoke();
+        }
+
+        public IEnumerator PlaySpriteProcess(Sprite sprite)
+        {
+            image.sprite = sprite;
+            cutsceneAnimator.Play("Start");
+
+            while (true)
+            {
+                AnimatorStateInfo stateInfo = cutsceneAnimator.GetCurrentAnimatorStateInfo(0);
+                if (stateInfo.IsName("Start") && stateInfo.normalizedTime < 1f)
+                {
+                    yield return null;
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,16 +5,32 @@ namespace GGJ23M
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField]
+        private GameStartProcess startProcess;
+
+        private bool isStartClicked = false;
 
         private void Start()
         {
             AudioPlayer.Instance.PlayMusic(0);
         }
 
-        public void ChangeScene()
+        public void OnStartClicked()
         {
+            if (!isStartClicked)
+            {
+                isStartClicked = true;
+                startProcess.Finished += ChangeScene;
+
+                gameObject.SetActive(false);
+                startProcess.Do();
+            }
+        }
+
+        private void ChangeScene()
+        {
+            startProcess.Finished -= ChangeScene;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-
 }
